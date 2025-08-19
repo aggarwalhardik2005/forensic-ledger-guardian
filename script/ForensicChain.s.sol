c12f0d84eb9a1b1e1ea504a61ad8042d5e74c778
aggarwalhardik2005 <hardikaggarwal01@gmail.com>
2025-08-19 22:03:46 +0530

    contract deployed

--- script/ForensicChain.s.sol
+++ script/ForensicChain.s.sol
@@ -1,18 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.29;
 
 import {Script} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {ForensicChain} from "../src/ForensicChain.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract ForensicChainScript is Script {
+    ForensicChain public forensicChain;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        forensicChain = new ForensicChain();
 
         vm.stopBroadcast();
     }
